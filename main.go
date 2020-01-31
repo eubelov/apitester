c9247f3808bbf106db20acaf6b83a8a66e1038d5
evgeny.belov <evgeny.belov@vista.co>
2020-01-31 15:45:52 +1300

    Output requests durations to a file

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"sync"
+	"time"
 
 	"golang.org/x/sync/semaphore"
 )
@@ -14,8 +16,10 @@ import (
 func main() {
 	failed := make(chan string)
 	urls := make([]string, 0)
-	var wg sync.WaitGroup
-	sem := semaphore.NewWeighted(50)
+	callTimes := make(chan time.Duration)
+
+	var wg, writers sync.WaitGroup
+	sem := semaphore.NewWeighted(10)
 
 	file, _ := os.Open(".\\testUrls.txt")
 	defer file.Close()
@@ -33,15 +37,40 @@ func main() {
 	for _, url := range urls {
 		go func(url string) {
 			sem.Acquire(context.TODO(), 1)
-			callAPI(url, failed, &wg)
+
+			callTimes <- callAPI(url, failed)
+
 			sem.Release(1)
 			processed++
 
 			fmt.Printf("\rProcessed: %d", processed)
+
+			wg.Done()
 		}(url)
 	}
 
 	go func() {
+		writers.Add(1)
+
+		f, err := os.Create(".\\requestsDurations.txt")
+		if err != nil {
+			return
+		}
+		defer f.Close()
+
+		fileWriter := bufio.NewWriter(f)
+		defer fileWriter.Flush()
+
+		for f := range callTimes {
+			fileWriter.WriteString(strconv.FormatInt(f.Milliseconds(), 10) + "\n")
+		}
+
+		writers.Done()
+	}()
+
+	go func() {
+		writers.Add(1)
+
 		errorsFile, err := os.Create(".\\error.txt")
 		if err != nil {
 			return
@@ -49,16 +78,26 @@ func main() {
 		defer errorsFile.Close()
 
 		fileWriter := bufio.NewWriter(errorsFile)
+		defer fileWriter.Flush()
+
 		for f := range failed {
 			fileWriter.WriteString(f + "\n")
 		}
+
+		writers.Done()
 	}()
 
 	wg.Wait()
+
 	close(failed)
+	close(callTimes)
+
+	writers.Wait()
 }
 
-func callAPI(url string, failed chan<- string, wg *sync.WaitGroup) {
+func callAPI(url string, failed chan<- string) time.Duration {
+	start := time.Now()
+
 	resp, err := http.Get(url)
 
 	if err == nil {
@@ -71,5 +110,7 @@ func callAPI(url string, failed chan<- string, wg *sync.WaitGroup) {
 		failed <- url
 	}
 
-	wg.Done()
+	duration := time.Since(start)
+
+	return duration
 }
